35a3213092ecac1ee16192dc3cc80e71556b96c3
Your Name <you@example.com>
2026-07-02 22:56:36 +0600

    Reorganize: Move stacks under Stack folder with Array and Linked List implementations

--- factorial/factorial.c
+++ factorial/factorial.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
+// Recursive function to calculate factorial
 long long factorial(int n) {
-    if (n < 0) return -1;
-    if (n == 0) return 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    }
     return n * factorial(n - 1);
 }
 
-int main(void) {
-    int n;
-    printf("Enter n: ");
-    if (scanf("%d", &n) != 1) return 1;
-    long long res = factorial(n);
-    if (res < 0) printf("Factorial undefined for negative n\n");
-    else printf("Factorial(%d) = %lld\n", n, res);
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d = %lld\n", num, factorial(num));
+    }
+
     return 0;
 }
